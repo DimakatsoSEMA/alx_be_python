89b575bf05f6b4b1bab06e765c37b651e8608960
Dimakatso Sema <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-05-27 10:03:10 +0200

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,7 +4,7 @@ user = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 if user == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif user == "rainy":
-    print("Don'/t forget your umbrella and a raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif user == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else: 
