e5a032fc8532de681328a4dae2babdad176589eb
Dimakatso Sema <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-05-28 09:11:05 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ user = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1,11):
     result = user * i
-    print(f"{user} x {i} = {result}")
+    print(f"{user} * {i} = {result}")
