518715f1b3b0025be48965c01a71a35ebe824ce3
Dimakatso <dimakatsosema@gmail.com>
2025-06-20 10:53:09 +0200

    edited library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,35 +1,41 @@
 class Book:
-   def __init__(self, title, author):
-       self.title = title
-       self.author = author
-   def get_details(self):
-       return f"Title: {self.title}, Author: {self.author}"
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
+    def __str__(self):
+        return f"Book: '{self.title}' by {self.author}"
 
-class EBook(Book):
-    def __init__(self, title, author, file_name): 
+class Ebook(Book):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_name = file_name
-    def get_details(self):
-        return f"{super().get_details()}, File Size: {self.file_size}MB"
+        self.file_size = file_size
 
+    def __str__(self):
+        return f"Ebook: '{self.title}' by {self.author}, File Size: {self.file_size}MB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-    def get_details(self):
-        return f"{super().get_details()}, Pages: {self.page_count}"
 
+    def __str__(self):
+        return f"Print Book: '{self.title}' by {self.author}, Pages: {self.page_count}"
 
 class Library:
     def __init__(self):
         self.books = []
+
     def add_book(self, book):
         if isinstance(book, Book):
             self.books.append(book)
         else:
             print("Only Book instances can be added.")
+
+    def list_books(self):
+        for book in self.books:
+            print(book)  
+
 
 
 
